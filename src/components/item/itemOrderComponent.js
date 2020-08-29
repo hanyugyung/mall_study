ccc35f4fd768df3bd80602ffafe5eab9c043e062
leesomi <122369@naver.com>
2020-08-29 17:59:43 +0900

    상품상세정보 폼 수정

--- src/components/item/itemOrderComponent.js
+++ src/components/item/itemOrderComponent.js
@@ -1,50 +1,71 @@
 import React, { Component } from "react";
+import { Button } from "react-bootstrap";
 
 class itemOrderComponent extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        console.log(this.props.prodData);
-        return (
-            <div className="item_order">
-                <div className="item_order_info">
-                    <span>{this.props.prodData.prodName}[{this.props.prodData.prodCode}]</span>
-                    <br/>
-                    <table>
-                        <tr className="sell">
-                            <td>판매가</td>
-                            <td>{this.props.prodData.sellPrice}원</td>
-                        </tr>
-                        <tr className="lines"></tr>
-                        <tr>
-                            <td>color</td>
-                            <td>select Box로</td>
-                        </tr>
-                        <tr>
-                            <td>size</td>
-                            <td>select Box로</td>
-                        </tr>
-                        <tr className="lines"></tr>
-                        <tr>
-                            <td colSpan="2">
-                            (최소주문수량 1개 이상)<br/>
-                            <span className="warning">위 옵션선택 박스를 선택하시면 아래에 상품이 추가됩니다.</span>
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-                <div className="payment">
-                    총 상품금액(수량) : <span>{this.props.prodData.sellPrice}</span>
-                </div>
-                <div>
-                    <button>Cart</button>
-                    <button>BUY</button>
-                </div>
-            </div>
-        );
-    };
-};
+  render() {
+    console.log(this.props.prodData);
+    return (
+      <div className="item_order">
+        <div className="item_order_info">
+          <span>
+            {this.props.prodData.prodName}[{this.props.prodData.prodCode}]
+          </span>
+          <br />
+          <table>
+            <tbody>
+              <tr className="sell">
+                <th>판매가</th>
+                <td>{this.props.prodData.sellPrice}원</td>
+              </tr>
+              <tr className="lines"></tr>
+              <tr>
+                <th>색상</th>
+                <td>
+                  <select name="color" defaultValue={""}>
+                    <option value="">선택</option>
+                    <option value="1">흰색</option>
+                    <option value="2">검정</option>
+                  </select>
+                </td>
+              </tr>
+              <tr>
+                <th>사이즈</th>
+                <td>
+                  <select name="size" defaultValue={""}>
+                    <option value="">선택</option>
+                    <option value="1">S</option>
+                    <option value="2">M</option>
+                    <option value="3">L</option>
+                  </select>
+                </td>
+              </tr>
+              <tr className="lines"></tr>
+              <tr>
+                <th colSpan="2">
+                  (최소주문수량 1개 이상)
+                  <br />
+                  <span className="warning">
+                    위 옵션선택 박스를 선택하시면 아래에 상품이 추가됩니다.
+                  </span>
+                </th>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className="payment">
+          총 상품금액(수량) : <span>{this.props.prodData.sellPrice}</span>
+        </div>
+        <div>
+          <Button variant="outline-dark">장바구니</Button>{" "}
+          <Button variant="outline-dark">구매하기</Button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default itemOrderComponent;

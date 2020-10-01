d9b54a8ac0db9b6a43def8fa091925455ca01431
ojwman <ohjae8851@naver.com>
2020-10-01 18:59:10 +0900

    add stat grid

--- src/components/stat/StatComponent.js
+++ src/components/stat/StatComponent.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
 import { Container } from "react-bootstrap";
+import { Grid } from 'gridjs-react';
+import "../../css/gridjs.css";
 
 class StatComponent extends Component {
   render() {
     return (
       <div className="sub-page">
-        <Container>통계</Container>
+        <Grid
+          data={[
+            ['John', 'john@example.com'],
+            ['Mike', 'mike@gmail.com']
+          ]}
+          columns={['Name', 'Email']}
+          search={true}
+          pagination={{
+            enabled: true,
+            limit: 1,
+          }}
+        />
       </div>
     );
   }
